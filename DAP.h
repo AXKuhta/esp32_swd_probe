7cfe16a9d44b546cca57368ace7f5fdaadffdaf8
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-08-01 01:43:39 +0800

    Improve clock generation, observing ~100 kHz at SWCLK now

--- DAP.h
+++ DAP.h
@@ -276,7 +276,7 @@ extern void     DAP_Setup (void);
 
 // Configurable delay for clock generation
 #ifndef DELAY_SLOW_CYCLES
-#define DELAY_SLOW_CYCLES       3U      // Number of cycles for one iteration
+#define DELAY_SLOW_CYCLES       20U      // Number of cycles for one iteration
 #endif
 static __forceinline void PIN_DELAY_SLOW (uint32_t delay) {
   volatile uint32_t count = delay;
